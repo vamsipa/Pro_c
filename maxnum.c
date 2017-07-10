5fd95e4d5cc2143a33c121bff221f35122a7cec0
Vamsi Pallapothula <vamsipallapothula@gmail.com>
2017-07-09 19:42:37 -0700

    Update maxnum.c

--- maxnum.c
+++ maxnum.c
@@ -9,7 +9,6 @@ int main()
   
   int num[4],
       max;
-
   
   //get 4 integers as num array
   puts("Enter 4 integers");
@@ -17,28 +16,21 @@ int main()
   for(int i=0; i<4; i++)
     {
       scanf("%d",&num[i]);
-
     }
 
   //assume first ineger as maximum value
   max=num[0];
-
   //compare max with second integ in array and if greater swap to max to get greatest integer in max
-  
   for(int j=1; j<4; j++)
     {
-      
       if(num[j]>max)
 	max=num[j];
       else
 	continue;
-	
-      
     }
   
   //print max num  
   printf("\n Maximum Integer is : %d",max);
-
 
   return 0;
 }
