04ac2960110df14cc6678ec3c2ebbee0895df344
Vamsi Pallapothula <vamsipallapothula@gmail.com>
2017-07-09 19:40:58 -0700

    Update arsize.c

--- arsize.c
+++ arsize.c
@@ -13,19 +13,15 @@
 #include<math.h>
 #include<stdlib.h>
 
-
 ///////////// Functions Prototyping
 
 int main()
 {
-
   int num[]= {10,20,30,40,50,50};
   int *ptr=NULL;
-      
 
   puts("number of elements in array are:  ");
   printf("  %d",(sizeof(num)/sizeof(num[0])));
 
   return 0;
-  
 }
